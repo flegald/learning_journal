50a1c94bc31924702a64032f0cc10658b84f431a
Michael Stokley <michael@debianT430.stokley>
2016-03-18 19:53:27 -0700

    view unit tests working

--- learning_journal/learning_journal/tests/test_views.py
+++ learning_journal/learning_journal/tests/test_views.py
@@ -5,6 +5,11 @@ from .conftest import DBSession
 from ..views import (list_view, single_entry_view)
 from ..models import Entry
 
+ENTRIES = [
+    ('title 1', 'text 1'),
+    ('title 2', 'text 2'),
+]
+
 
 # def test_list(loaded_db):
 #     return_dict = list_view(req)
@@ -13,13 +18,16 @@ from ..models import Entry
 #     # assert list(req) == {}
 
 
-def test_entry_view_text(dbtransaction):
+@pytest.mark.parametrize(('title', 'text'), ENTRIES)
+def test_entry_view_text(title, text, dbtransaction):
     """Test for entry view dictionary text attribute."""
-    new_model = Entry(title='thefreshloaf', text='text about fresh loaves')
-    DBSession.add(new_model)
+    entry = Entry(title=title, text=text)
+    DBSession.add(entry)
     DBSession.flush()
-    test_request = testing.DummyRequest()
-    test_request.matchdict = {'id': new_model.id}
-    dic = list_view(test_request)
-    print(dic['entries'])
-    assert dic['entries'].id == 'text about fresh loaves'
+    req = testing.DummyRequest()
+    req.matchdict = {'id': entry.id}
+    dic = list_view(req)
+    # TODO: test both attributes in one assert statement
+    # NOTE: for attribute in [text, title] does not work
+    assert dic['entries'][0].text == text
+    assert dic['entries'][0].title == title
